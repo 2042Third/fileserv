279431dcc72ed69565747c820386f9009c7bc8d3
mike yang <18604713262@163.com>
2021-10-26 14:54:55 -0400

    script commit

--- WEB-INF/classes/Upload.java
+++ WEB-INF/classes/Upload.java
@@ -87,7 +87,7 @@ public class Upload extends HttpServlet {
         final ServletContext servletContext = getServletContext();
         try {
 
-            List<String> list_files = servletContext.getResourcePaths("/file-saved");
+            Set<String> list_files = servletContext.getResourcePaths("/file-saved");
             request.setAttribute("list_files", list_files);
 
             RequestDispatcher dispatcher = request.getRequestDispatcher("download");
