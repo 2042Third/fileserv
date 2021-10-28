6d2d1fecbbe28236a7fc4e814d2cdc0feb4381d1
mike yang <18604713262@163.com>
2021-10-28 14:59:40 -0400

    script commit

--- WEB-INF/classes/Upload.java
+++ WEB-INF/classes/Upload.java
@@ -67,7 +67,7 @@ public class Upload extends HttpServlet {
                 case "user_name":
                     savePath = set_up_user_path(part,savePath);
                     continue;
-                case "file_listing ":
+                case "file_listing":
                     System.out.println("[FileListing] received.");
                     serv_type=ServiceType.FILELISTING;
                     continue;
